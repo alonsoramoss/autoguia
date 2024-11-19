80f816e0e494f667f8eab6af7bbcaf0139d82172
Alonso Ramos <alonso24005@hotmail.com>
2024-11-18 23:32:42 -0500

    refactor: reorganiza los script

---
+++ js/app.js
@@ -0,0 +1,13 @@
+document.querySelectorAll('nav a, footer a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+
+        const target = document.querySelector(this.getAttribute('href'));
+        const offset = window.innerWidth <= 768 ? 50 : 100;
+
+        window.scrollTo({
+            top: target.offsetTop - offset,
+            behavior: 'smooth'
+        });
+    });
+});
---
+++ js/form.js
@@ -0,0 +1,31 @@
+const form = document.getElementById('contactoForm');
+const mensajeEstado = document.getElementById('mensajeEstado');
+
+form.addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const formData = new FormData(form);
+    fetch(form.action, {
+        method: form.method,
+        body: formData,
+        headers: {
+            'Accept': 'application/json'
+        }
+    })
+    .then(response => {
+        if (response.ok) {
+            mensajeEstado.style.display = 'block';
+            form.reset();
+        } else {
+            alert('Hubo un problema al enviar el formulario. Por favor, inténtelo de nuevo.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Ocurrió un error inesperado. Por favor, inténtelo de nuevo más tarde.');
+    });
+
+    setTimeout(() => {
+        mensajeEstado.style.display = 'none';
+    }, 6000);
+});
