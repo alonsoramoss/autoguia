eb92d3c4f690227eff505da2f6a7274e986cf599
Alonso Ramos <alonso24005@hotmail.com>
2024-12-03 23:09:39 -0500

    chore: estandariza comillas

--- js/form.js
+++ js/form.js
@@ -1,7 +1,7 @@
-const form = document.getElementById('contactoForm');
-const mensajeEstado = document.getElementById('mensajeEstado');
+const form = document.getElementById("contactoForm");
+const mensajeEstado = document.getElementById("mensajeEstado");
 
-form.addEventListener('submit', function(event) {
+form.addEventListener("submit", function(event) {
     event.preventDefault();
 
     const formData = new FormData(form);
@@ -9,23 +9,23 @@ form.addEventListener('submit', function(event) {
         method: form.method,
         body: formData,
         headers: {
-            'Accept': 'application/json'
+            "Accept": "application/json"
         }
     })
     .then(response => {
         if (response.ok) {
-            mensajeEstado.style.display = 'block';
+            mensajeEstado.style.display = "block";
             form.reset();
         } else {
-            alert('Hubo un problema al enviar el formulario. Por favor, inténtelo de nuevo.');
+            alert("Hubo un problema al enviar el formulario. Por favor, inténtelo de nuevo.");
         }
     })
     .catch(error => {
-        console.error('Error:', error);
-        alert('Ocurrió un error inesperado. Por favor, inténtelo de nuevo más tarde.');
+        console.error("Error:", error);
+        alert("Ocurrió un error inesperado. Por favor, inténtelo de nuevo más tarde.");
     });
 
     setTimeout(() => {
-        mensajeEstado.style.display = 'none';
+        mensajeEstado.style.display = "none";
     }, 6000);
 });
